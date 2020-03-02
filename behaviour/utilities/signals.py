fee3cc5d6bc031f5dbc5364d8bc4cfa06880cb66
FedeClaudi <federicoclaudi@protonmail.com>
2020-03-02 11:35:14 +0000

    improved signal onset detection

--- behaviour/utilities/signals.py
+++ behaviour/utilities/signals.py
@@ -19,7 +19,7 @@ def get_frames_times_from_squarewave_signal(squarewave_signal, th=4,):
     return np.where(derivative > th)[0]
 
 
-def get_times_signal_high_and_low(signal, th=1):
+def get_times_signal_high_and_low(signal, th=1, min_time_between_highs=None):
     """
         Given a 1d time series it returns the times 
         (in # samples) in which the signal goes low->high (onset)
@@ -27,10 +27,20 @@ def get_times_signal_high_and_low(signal, th=1):
 
         :param signal: 1d numpy array or list with time series data
         :param th: float, the time derivative of signal is thresholded to find onset and offset
+        :param min_time_between_highs: int, if a number of passes, onsets that are closer in time
+                than this value are ignored.
     """
     signal_copy = np.zeros_like(signal)
     signal_copy[signal > th] = 1
 
     signal_onset = np.where(np.diff(signal_copy) > .5)[0]
+    if min_time_between_highs is not None:
+        signal_onset = np.concatenate([[signal_onset[0]], 
+                            signal_onset[np.where(np.diff(signal_onset)>min_time_between_highs)[0]]])
+
+
     signal_offset = np.where(np.diff(signal_copy) < -.5)[0]
+    if min_time_between_highs is not None:
+        signal_offset = np.concatenate([[signal_offset[0]], 
+                            signal_offset[np.where(np.diff(signal_offset)>min_time_between_highs)[0]]])
     return signal_onset, signal_offset
